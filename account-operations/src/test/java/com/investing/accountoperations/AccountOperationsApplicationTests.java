576d3743dd649dac5180bd4d5ee260ec683309a1
OsmarVitor <perez_vitor@hotmail.com>
2023-01-20 16:47:02 -0300

    update

---
+++ account-operations/src/test/java/com/investing/accountoperations/AccountOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investing.accountoperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gateway/src/main/java/com/investing/gateway/GatewayApplication.java
@@ -0,0 +1,10 @@
+//package com.investing.gateway;
+//
+//@SpringBootApplication
+//public class GatewayApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(GatewayApplication.class, args);
+//	}
+//
+//}
